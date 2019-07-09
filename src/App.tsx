6e3075fcfb6679e7ff47e0d01445bd9c407a8d88
shaun sweet <shaun@shaunsweet.com>
2019-07-09 00:30:25 -0700

    add my own boilerplate cruft :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import reset from 'styled-reset';
+import Layout from './features/common/Layout';
+import theme from './theme';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Layout />
+      </ThemeProvider>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/features/common/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { LayoutWrapper } from './Layout.styles';
+
+type OwnProps = {
+
+}
+
+const Layout: React.FC<OwnProps> = (props) => (
+  <LayoutWrapper>
+    This is a Layout
+  </LayoutWrapper>
+);
+
+export default Layout;
